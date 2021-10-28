9092b60c69460ab733b2045459031e020dc63bbe
Sergey Peshkov <zloi.g1rz@gmail.com>
2021-10-29 01:40:30 +0800

    Начал форму добавления пользователя

--- src/components/ModalAdd/ModalAdd.jsx
+++ src/components/ModalAdd/ModalAdd.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React from 'react';
 import ModalWrap from '../ModalWrap/ModalWrap';
+
+import './ModalAdd.sass';
 
 const ModalAdd = () => {
     return (
         <ModalWrap title="Добавить пользователя">
-            test
+            <form className="form">
+                <div className="form-group">
+                    <label htmlFor="user_name" className="form-label">Имя:</label>
+                    <input id="user_name" type="text" name="name" className="form-input" />
+                </div>
+
+                <div className="form-group">
+                    <label htmlFor="user_pay" className="form-label">Траты:</label>
+                    <input id="user_pay" type="text" name="pay" className="form-input" />
+                </div>
+            </form>
         </ModalWrap>
     )
 }
