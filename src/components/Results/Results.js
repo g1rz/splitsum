543dcea8f0e11c8d5280ee09964f0a840f38aaf0
Sergey Peshkov <zloi.g1rz@gmail.com>
2021-11-03 13:28:40 +0800

    Вывод общей информации в результате

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -2,71 +2,79 @@ import React from 'react';
 import ColumnItem from '../ColumnItem/ColumnItem';
 import ResultsItem from '../ResultsItem/ResultsItem';
 
+import './Results.sass';
+
 const Results = ({ users }) => {
 
+    const [sum, setSum] = React.useState(0);
+    const [averageSum, setAverageSum] = React.useState(0);
+    const [results, setResults ] = React.useState([]);
 
-    let debtors = [],
-        creditors = [],
-        results = [];
 
-    let sum = users.reduce((sum, item) => sum += item.pay, 0);
-    let averageSum = Math.round(sum / users.length * 10) / 10;
+    React.useEffect(() => {
+        let debtors = [],
+            creditors = [],
+            results = [];
 
-    console.log('sum', sum);
-    console.log('averageSum', averageSum);
-    users.map( (item) => {
-        let payDiff = item.pay - averageSum;
-        payDiff = Math.round(payDiff * 100) / 100;
-        let newItem = {...item};
-         
-        if ( payDiff > 0 ) {
-            newItem.payDiff = payDiff;
-            creditors.push(newItem);
-        } else {
-            newItem.payDiff = Math.abs(payDiff);
-            debtors.push(newItem);
-        }
-    });
+        let sum = users.reduce((sum, item) => sum += item.pay, 0);
+        let averageSum = Math.round(sum / users.length * 10) / 10;
 
-    console.log('debtors');
-    debtors.map(item => console.log(item));
-    console.log('creditors');
-    creditors.map(item => console.log(item));
+        setSum(sum);
+        setAverageSum(averageSum);
 
-    while (creditors.length != 0 && debtors.length != 0) {
-        let currCreditor = creditors[0];
-        let currDebtor = debtors[0];
-        let diff = 0;
-        let transaction = 0;
-        if (currCreditor.payDiff < currDebtor.payDiff) {
-            transaction = currCreditor.payDiff;
-            diff = currDebtor.payDiff - currCreditor.payDiff;
-            diff = Math.round(diff * 10) / 10;
-            currCreditor.payDiff = 0;
-            currDebtor.payDiff = diff;
-            creditors.shift();
-            
-        } else if (currCreditor.payDiff > currDebtor.payDiff) {
-            transaction = currDebtor.payDiff;
-            diff = currCreditor.payDiff - currDebtor.payDiff;
-            diff = Math.round(diff * 10) / 10;
-            currCreditor.payDiff = diff;
-            currDebtor.payDiff = 0;
-            debtors.shift();
-        } else {
-            transaction = currCreditor.payDiff;
-            diff = currDebtor.payDiff;
-            currCreditor.payDiff = 0;
-            currDebtor.payDiff = 0;
-            creditors.shift();
-            debtors.shift();
-        } 
-        results.push({
-            debtorName: currDebtor.name,
-            creditorName: currCreditor.name,
-            transaction: transaction.toFixed(2)
+        users.map( (item) => {
+            let payDiff = item.pay - averageSum;
+            payDiff = Math.round(payDiff * 100) / 100;
+            let newItem = {...item};
+             
+            if ( payDiff > 0 ) {
+                newItem.payDiff = payDiff;
+                creditors.push(newItem);
+            } else {
+                newItem.payDiff = Math.abs(payDiff);
+                debtors.push(newItem);
+            }
         });
-    }
+
+        while (creditors.length != 0 && debtors.length != 0) {
+            let currCreditor = creditors[0];
+            let currDebtor = debtors[0];
+            let diff = 0;
+            let transaction = 0;
+            if (currCreditor.payDiff < currDebtor.payDiff) {
+                transaction = currCreditor.payDiff;
+                diff = currDebtor.payDiff - currCreditor.payDiff;
+                diff = Math.round(diff * 10) / 10;
+                currCreditor.payDiff = 0;
+                currDebtor.payDiff = diff;
+                creditors.shift();
+             
+            } else if (currCreditor.payDiff > currDebtor.payDiff) {
+                transaction = currDebtor.payDiff;
+                diff = currCreditor.payDiff - currDebtor.payDiff;
+                diff = Math.round(diff * 10) / 10;
+                currCreditor.payDiff = diff;
+                currDebtor.payDiff = 0;
+                debtors.shift();
+            } else {
+                transaction = currCreditor.payDiff;
+                diff = currDebtor.payDiff;
+                currCreditor.payDiff = 0;
+                currDebtor.payDiff = 0;
+                creditors.shift();
+                debtors.shift();
+            } 
+            results.push({
+                debtorName: currDebtor.name,
+                creditorName: currCreditor.name,
+                transaction: transaction.toFixed(2)
+            });
+        };
+
+        setResults(results);
+    }, [users]);
+
+    
 
     const resultList = results ? results.map((item, index) => {
         return (
@@ -82,7 +90,32 @@ const Results = ({ users }) => {
     return (
         <ColumnItem 
             title="Результат">
-            {resultList}
+            <div className="result-list">
+                {resultList}
+            </div>
+            
+            {sum > 0 && averageSum > 0 && (
+                <div className="result-general">
+                    <div class="user-info">
+                        <div class="user-info__pay">
+                            <span class="user-info__item">{sum} руб.</span>
+                        </div>
+                        <div class="user-info__name">
+                            <span class="user-info__item">Итого</span>
+                        </div>
+                    </div>
+
+                    <div class="user-info">
+                        <div class="user-info__pay">
+                            <span class="user-info__item">{averageSum} руб.</span>
+                        </div>
+                        <div class="user-info__name">
+                            <span class="user-info__item">Среднее</span>
+                        </div>
+                    </div>
+                </div>
+            )}
+            
         </ColumnItem>
     )
 }
