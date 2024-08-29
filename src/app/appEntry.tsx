f7b421432fcd1671ac2c8a599f095d1cb7298828
Sergey Peshkov <zloi.g1rz@gmail.com>
2024-08-29 13:21:10 +0800

    Выносит базовые стили в shared

--- src/app/appEntry.tsx
+++ src/app/appEntry.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './app/App';
+import App from './App';
+
+import '~/shared/base.sass';
 
 ReactDOM.render(
     <React.StrictMode>
---
+++ src/pages/calc/index.ts
@@ -0,0 +1,1 @@
+export { CalcPage } from './ui/Page/Page';
