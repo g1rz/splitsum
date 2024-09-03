e9fb8f77e84e805350a71855f4e5b7479a995946
Sergey Peshkov <zloi.g1rz@gmail.com>
2024-09-03 23:55:54 +0800

    Добавляет брендирование типов

--- types/app.d.ts
+++ types/app.d.ts
@@ -1,9 +1,17 @@
 import { ReactNode } from 'react';
+declare const _brand: unique symbol;
 
 declare global {
     export type TChildReactNode = {
         children?: ReactNode;
     };
+
+    export type Brand<K, T> = K & { [_brand]: T };
+
+    export type Id = number;
+
+    declare type RootState = import('../src/app/appStore').RootState;
+    declare type AppDispatch = import('../src/app/appStore').AppDispatch;
 }
 
 export {};
