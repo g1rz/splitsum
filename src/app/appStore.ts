2d032c62da44e69685e262b6151b2a85831f9ddf
Sergey Peshkov <zloi.g1rz@gmail.com>
2024-09-03 23:56:31 +0800

    Добавляет редьюсеры в стор

--- src/app/appStore.ts
+++ src/app/appStore.ts
@@ -1,14 +1,20 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { useDispatch } from 'react-redux';
+import { expenseSlice } from '~/entities/expenses/model/slice';
+import { productsSlice } from '~/entities/products';
+import { usersSlice } from '~/entities/user';
 
 const store = configureStore({
     reducer: {
-        // Здесь будут добавлены ваши reducers
+        users: usersSlice.reducer,
+        expenses: expenseSlice.reducer,
+        products: productsSlice.reducer,
     },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 
 export default store;
