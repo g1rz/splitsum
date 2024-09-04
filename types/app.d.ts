82cb1a6882d826ba1967458b5d7f0ad37f6bb2de
Sergey Peshkov <zloi.g1rz@gmail.com>
2024-09-04 16:54:06 +0800

    Правит тип Id

--- types/app.d.ts
+++ types/app.d.ts
@@ -8,7 +8,7 @@ declare global {
 
     export type Brand<K, T> = K & { [_brand]: T };
 
-    export type Id = number;
+    export type Id = string;
 
     declare type RootState = import('../src/app/appStore').RootState;
     declare type AppDispatch = import('../src/app/appStore').AppDispatch;
